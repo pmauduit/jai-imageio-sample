322d3397a6d9a6d3c394d1d44514c6944b082e8a
Pierre Mauduit <pierre.mauduit@gmail.com>
2014-04-21 18:06:42 +0200

    Some revamp on the GdalOgrJni controller

--- jai-imageio-gdal-sample-webapp/src/main/java/com/camptocamp/GdalOgrJNIController.java
+++ jai-imageio-gdal-sample-webapp/src/main/java/com/camptocamp/GdalOgrJNIController.java
@@ -5,7 +5,6 @@ import it.geosolutions.imageio.gdalframework.GDALUtilities;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Iterator;
-import java.util.Vector;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -30,20 +29,6 @@ public class GdalOgrJNIController {
 
 	
 	static Logger log = Logger.getLogger(GdalOgrJNIController.class.getName());
-
-	static boolean turboJpegEnabled = false;
-	
-	static boolean isTurboJpegEnabled() {
-		try {
-  			System.loadLibrary("turbojpeg");
-			turboJpegEnabled = true;
-		} catch (Throwable e) {
- 			log.info("[error] Loading turboJPEG: " + e.getMessage());
-		}
-
-		return turboJpegEnabled;
-		
-	}
 	
 	protected JSONObject getGDALOGRStatus() {
 		JSONObject r = new JSONObject();
@@ -56,9 +41,14 @@ public class GdalOgrJNIController {
 			gtDatastores.put(dsfspi.getDisplayName());
 		}
 		r.put("datastores", gtDatastores);
-		// OGR / GeoTools
+		// OGR
 		try {
+			Class.forName("org.gdal.ogr.ogr");
+
 			OGRDataStoreFactory f = new JniOGRDataStoreFactory();
+
+			// Next step will call loadLibrary("gdaljni") ... Not good.
+			// This will arise in GeoTools library.
 
 			if (f.isAvailable()) {
 				JSONArray availableOGRDrivers = new JSONArray();
@@ -75,20 +65,36 @@ public class GdalOgrJNIController {
 			else {
 				r.put("OGR",
 						new JSONObject().put("status", "unavailable").put(
-								"reason", "Probable misconfiguration"));
+								"reason", "JniOGR reported as unavailable (JniOGRDataStoreFactory.doIsAvailable() throwed "
+										+ "something or returned false, presumably native library not found in the java.library.path "
+										+ "or attempted to be loaded twice."));
 			}
 			// Exception raised
+		} catch (ClassNotFoundException e){
+			r.put("OGR",
+					new JSONObject().put("status", "unavailable").put(
+							"reason", "Unable to resolve class org.gdal.ogr.ogr. Probable misconfiguration of the classpath."));			
 		} catch (Throwable e) {
 			r.put("OGR",
 					new JSONObject().put("status", "unavailable").put("reason",
 							e.toString()));
 		}
 
-		// GDAL		
-		if (GDALUtilities.isGDALAvailable()) {
-			r.put("GDAL", new JSONObject().put("status", "available"));
-		} else {
-			r.put("GDAL", new JSONObject().put("status", "unavailable"));
+		// GDAL
+		try {
+			// Same remark: Call to loadLibrary("gdaljni") initiated 
+			// by imageIO-Ext library ("vanilla" version, my fork on github
+			// avoids this).
+			if (GDALUtilities.isGDALAvailable()) {
+				r.put("GDAL", new JSONObject().put("status", "available"));
+			} else {
+				r.put("GDAL", new JSONObject().put("status", "unavailable"));
+			}
+		} catch (NoClassDefFoundError e) {
+			r.put("GDAL", new JSONObject().put("status", "unavailable")
+					.put("reason", "Unable to find class org.gdal.gdal.gdal, this is likely due to "
+							+ "a classpath misconfiguration."));
+			
 		}
 
 		return r;
@@ -106,16 +112,44 @@ public class GdalOgrJNIController {
 			// Add GeoTools informations
 			info.put("geotools", getGDALOGRStatus());
 			// JAI
+			try {
 			info.put("jai-imageio",
 					new JSONObject().put("status", Image.isAvailable() ? "available" : "unavailable" ));
 
+			} catch (NoClassDefFoundError e) {
+				info.put("jai-imageio",
+						new JSONObject().put("status", "unavailable" ).put("reason",
+								"Unable to find class com.sun.medialib.mlib.Image, this is likely due to "
+										+ "a classpath misconfiguration."));
+				
+			}
 			// TurboJPEG
-			info.put("libturbojpeg",
-					new JSONObject().put("status", GdalOgrJNIController.isTurboJpegEnabled() ? "available" : "unavailable"));
+			try {
+				Class c = Class
+						.forName("org.libjpegturbo.turbojpeg.TJCompressor");
+				c.newInstance();
+			} catch (NoClassDefFoundError e) {
+				info.put("libturbojpeg",
+						new JSONObject().put("status", "unavailable").put("reason", 
+								"Unable to find class org.libjpegturbo.turbojpeg.TJCompressor, this is likely due to "
+										+ "a classpath misconfiguration."));
+			} catch (ClassNotFoundException e) {
+				info.put("libturbojpeg",
+				new JSONObject().put("status", "unavailable").put("reason", 
+						"Unable to find class org.libjpegturbo.turbojpeg.TJCompressor, this is likely due to "
+								+ "a classpath misconfiguration."));				
+			} catch (UnsatisfiedLinkError e) {
+				info.put("libturbojpeg",
+				new JSONObject().put("status", "unavailable").put("reason", 
+						"Class found but references unreachable native code. Probably a mismatch between "
+						+ "bindings and native library version."));					
+			} catch (Throwable e) {
+				info.put("libturbojpeg",
+						new JSONObject().put("status", "unavailable").put("reason", e.getMessage()));
+			}
 
-		
 		} catch (Throwable e) {
-			info.put("exception", e.getMessage());
+			info.put("unhandled_exception", e.getMessage());
 		} 
 		finally {
 			ret.write(info.toString(2));
